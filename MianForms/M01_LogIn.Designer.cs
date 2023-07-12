d1782dea6e24851a97408db78b7c7d80fd3997ba
user <user@DESKTOP-Q580OO3>
2023-07-12 15:53:11 +0900

    초기 커밋.

---
+++ MianForms/M01_LogIn.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MianForms
+{
+    partial class M01_LogIn
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.btnPWChange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(99, 25);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(291, 25);
+            this.txtUserId.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "사용자 ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(99, 53);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(291, 25);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(161, 84);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(106, 49);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "로그인";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // btnPWChange
+            // 
+            this.btnPWChange.Location = new System.Drawing.Point(273, 84);
+            this.btnPWChange.Name = "btnPWChange";
+            this.btnPWChange.Size = new System.Drawing.Size(117, 49);
+            this.btnPWChange.TabIndex = 5;
+            this.btnPWChange.Text = "비밀번호 변경";
+            this.btnPWChange.UseVisualStyleBackColor = true;
+            // 
+            // M01_LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 155);
+            this.Controls.Add(this.btnPWChange);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserId);
+            this.Name = "M01_LogIn";
+            this.Text = "로그인";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Button btnPWChange;
+    }
+}
+
---
+++ MianForms/M01_LogIn.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MianForms
+{
+    public partial class M01_LogIn : Form
+    {
+        public M01_LogIn()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            // 로그인 버튼 을 클릭 했을때 진입점. 
+
+            // 벨리데이션 체크 
+            // . 개발자가 프로그램 로직을 수행하는 코딩을 하기 전에 
+            //    예외적으로 검출해야 하는 상황 또는 정상 로직이 수행 되지 않는 조건 을 
+            //    미리 예측하고 코딩에 적용하는 방법
+
+            string sUserId   = string.Empty;   // 사용자 ID  변수 등록
+            string sPassword = string.Empty;   // 비밀번호   변수 등록
+
+            // 사용자 ID 와 PW 를 정확히 입력 하였는지 확인
+            sUserId   = txtUserId.Text;   // 사용자 ID 변수에 사용자ID 텍스트 박스에 입력한 내용 할당
+            sPassword = txtPassword.Text; // 사용자 비밀번호 변후 세 사용자 비밀번호 텍스트박스에 입력한 내용 할당.
+
+            // 필수 입력 사항이 누락 되었을 경우 변수. 
+            string Message = string.Empty;
+            if (sUserId == "")
+            {
+                Message = "사용자 ID ";
+            }
+            else if (sPassword == "")
+            {
+                Message += "비밀번호 ";
+            }
+
+            if (Message != "")
+            {
+                MessageBox.Show($"{Message} 를 입력 하지 않았습니다.");
+                return;
+            }
+
+            // 1. 데이터 베이스 접속 경로 .     
+            string Sconnection = "Server = DESKTOP-Q580OO3\\MSSQLSERVER01; Uid = sa; Pwd = 1234; database = AppDev;";
+
+            // 2. 데이터 베이스 접속 할수 있는 클래스 
+            SqlConnection Connect = new SqlConnection(Sconnection);
+
+            // 정상 데이터베이스 접속
+            try
+            {
+                // 3. 데이터 베이스 open
+                Connect.Open();
+
+                // 4. 데이터 베이스에 전달할 명령 
+                string sSqlSelect = $"SELECT * FROM TB_USER WHERE USERID = '{sUserId}' AND PW = '{sPassword}'";
+
+                // 5. 명령을 전달 할 클래스
+                SqlDataAdapter adapter = new SqlDataAdapter(sSqlSelect, Connect);
+
+                // 6. 데이터 베이스 조회 수행 및 결과 반환. 
+                // DataTable : C# 에서 데이터 베이스의 반환 결과 를 할당 받을 수 있는 자료구조. 
+                DataTable dTtemp = new DataTable();
+                adapter.Fill(dTtemp); 
+
+                // 로그인 을 할 수 있는 사용자 인지 체크 
+                if (dTtemp.Rows.Count == 0)
+                {
+                    MessageBox.Show("사용자 ID 또는 비밀번호가 잘못되었습니다.");
+                    txtUserId.Text = "";   // 사용자 ID
+                    txtPassword.Text = ""; // 사용자 비밀번호 
+                    txtUserId.Focus(); // 커서 위치 를 활성화
+                    return;
+                }
+
+                // 정상 로그인 처리 로직.
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                // 데이터 베이스 접속 종료
+                Connect.Close(); 
+            }
+
+        }
+    }
+}
